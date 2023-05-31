09223d19206974d065a67dc92b18a1c1a3814c65
YUANG <109567724+aa103967622@users.noreply.github.com>
2023-05-31 18:07:50 +0800

    Update p03.py

--- exam/p/p03.py
+++ exam/p/p03.py
@@ -1,6 +1,23 @@
 def p03(x=123):
     reverse=None
     # ↓程式區域↓
+ reverse=None
+    # ↓程式區域↓
+    x = int(input("請輸入整數 x: "))
+    reverse = 0
+    is_negative = False
 
+    if x < 0:
+        is_negative = True
+        x = abs(x)
+
+    while x > 0:
+        digit = x % 10
+        reverse = (reverse * 10) + digit
+        x = x // 10
+
+    if is_negative:
+        reverse = -reverse
+    print("輸出結果:", reverse)
     # ↑程式區域↑
     return reverse
